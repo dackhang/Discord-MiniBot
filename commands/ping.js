942ee9827a1ecb6237134e0578f7d00b5f0dd679
Ness <154395849+dackhang@users.noreply.github.com>
2025-06-12 00:27:53 +0700

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replays with the bot ping!'),
+
+  async execute(interaction) {
+    await interaction.deferReply();
+
+    const reply = await interaction.fetchReply();
+    const ping = reply.createdTimestamp - interaction.createdTimestamp;
+
+    await interaction.editReply(`🏓 Pong! Client: ${ping}ms | WebSocket: ${interaction.client.ws.ping}ms`);
+  }
+};
